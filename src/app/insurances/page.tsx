1df76d5ba3c46261d116b8fe6811beb1fbc9087d
mahdighaffari123 <mahdi.ghaffari28@gmail.com>
2025-02-21 12:31:17 +0330

    feat: insurances

---
+++ src/app/insurances/page.tsx
@@ -0,0 +1,487 @@
+import { Divider } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="pt-[50px] pb-[120px] max-w-[1168px] mx-auto lg:px-0 px-5 flex flex-col gap-y-16">
+      <div className="flex flex-col gap-y-[30px]">
+        <h2 className="text-xl font-semibold lg:text-right text-center">
+          بیمه های مکمل طرف قرارداد با بیمارستان نیکان
+        </h2>
+        <div className="flex flex-col gap-y-[66px]">
+          <div className="md:bg-[#F6F6F6] bg-transparent rounded-[26px] lg:flex grid grid-cols-3 justify-items-center gap-y-8 justify-between lg:flex-nowrap flex-wrap items-center lg:px-[35px] py-3">
+            <Image
+              src={"/images/insurances/kar-afarin.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/asia.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/novin.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <div className="w-full col-span-3 lg:hidden block">
+              <Divider
+                orientation="horizontal"
+                className="!bg-[#A1A1A1] !w-full !h-[1px] lg:!hidden !block"
+              />
+            </div>
+            <Image
+              src={"/images/insurances/alborz.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/komak-resan.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/raze.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+          </div>
+          <div className="md:bg-[#F6F6F6] bg-transparent rounded-[26px] lg:flex grid grid-cols-3 justify-items-center gap-y-8 justify-between lg:flex-nowrap flex-wrap items-center lg:px-[35px] py-3">
+            <Image
+              src={"/images/insurances/atieh-sazan-hafez.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/dey.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/parsian.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <div className="w-full col-span-3 lg:hidden block">
+              <Divider
+                orientation="horizontal"
+                className="!bg-[#A1A1A1] !w-full !h-[1px] lg:!hidden !block"
+              />
+            </div>
+            <Image
+              src={"/images/insurances/tejarat-no.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/taavon.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/sahami.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+          </div>
+          <div className="md:bg-[#F6F6F6] bg-transparent rounded-[26px] lg:flex grid grid-cols-3 justify-items-center gap-y-8 justify-between lg:flex-nowrap flex-wrap items-center lg:px-[35px] py-3">
+            <Image
+              src={"/images/insurances/dana.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/mellat.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/moaalem.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <div className="w-full col-span-3 lg:hidden block">
+              <Divider
+                orientation="horizontal"
+                className="!bg-[#A1A1A1] !w-full !h-[1px] lg:!hidden !block"
+              />
+            </div>
+            <Image
+              src={"/images/insurances/ma.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/sina.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/sarmad.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-[30px]">
+        <h2 className="text-xl font-semibold lg:text-right text-center">
+          بیمه های بانک طرف قرارداد با بیمارستان نیکان
+        </h2>
+        <div className="flex flex-col gap-y-[66px]">
+          <div className="md:bg-[#F6F6F6] bg-transparent rounded-[26px] lg:flex grid grid-cols-3 justify-items-center gap-y-8 justify-between lg:flex-nowrap flex-wrap items-center lg:px-[35px] py-3">
+            <Image
+              src={"/images/insurances/refah-kargaran.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/markazi.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/sanat-madan.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <div className="w-full col-span-3 lg:hidden block">
+              <Divider
+                orientation="horizontal"
+                className="!bg-[#A1A1A1] !w-full !h-[1px] lg:!hidden !block"
+              />
+            </div>
+            <Image
+              src={"/images/insurances/keshavarzi.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/bank-mellat.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/tejarat.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-y-[66px]">
+          <div className="md:bg-[#F6F6F6] bg-transparent rounded-[26px] lg:flex grid grid-cols-3 justify-items-center gap-y-8 justify-evenly lg:flex-nowrap flex-wrap items-center lg:px-[35px] py-3">
+            <Image
+              src={"/images/insurances/tosee-saderat.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/maskan.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/sepah.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-y-[30px]">
+        <h2 className="text-xl font-semibold lg:text-right text-center">
+          بیمه های بیمه وزارتخانه ها ، سازمان ها و شرکت ها طرف قرارداد با
+          بیمارستان نیکان
+        </h2>
+        <div className="flex flex-col gap-y-[66px]">
+          <div className="md:bg-[#F6F6F6] bg-transparent rounded-[26px] lg:flex grid grid-cols-3 justify-items-center gap-y-8 justify-between lg:flex-nowrap flex-wrap items-center lg:px-[35px] py-3">
+            <Image
+              src={"/images/insurances/markaz-pazouhesh.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/melli-naft.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/majles-shora.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <div className="w-full col-span-3 lg:hidden block">
+              <Divider
+                orientation="horizontal"
+                className="!bg-[#A1A1A1] !w-full !h-[1px] lg:!hidden !block"
+              />
+            </div>
+            <Image
+              src={"/images/insurances/naft-falat.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/sandogh-bazneshastegi.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/ketabkhane.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+          </div>
+          <div className="md:bg-[#F6F6F6] bg-transparent rounded-[26px] lg:flex grid grid-cols-3 justify-items-center gap-y-8 justify-between lg:flex-nowrap flex-wrap items-center lg:px-[35px] py-3">
+            <Image
+              src={"/images/insurances/shora-negahban.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/bonyad-15-khordad.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/shahrdari-tehran.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <div className="w-full col-span-3 lg:hidden block">
+              <Divider
+                orientation="horizontal"
+                className="!bg-[#A1A1A1] !w-full !h-[1px] lg:!hidden !block"
+              />
+            </div>
+            <Image
+              src={"/images/insurances/ab-fazelab.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/sanaye-mes.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+            <Divider
+              orientation="vertical"
+              className="!bg-[#A1A1A1] !w-[1px] !h-[113px] lg:!block hidden"
+            />
+            <Image
+              src={"/images/insurances/mina.png"}
+              alt=""
+              width={111}
+              height={122}
+              className="object-contain h-[122px] w-[111px]"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;

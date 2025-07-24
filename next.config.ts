a68f9fa2375e58ec8e20179192ee68d004e2d8a5
AminEtemadifar <aminetemadifar.w@gmail.com>
2025-07-24 14:16:55 +0330

    update icons

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,11 @@ const nextConfig: NextConfig = {
         hostname: "api.mifadev.ir",
         port: "",
       },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+      },
     ],
   },
 
--- src/app/(home)/_components/nikan365Intro/index.tsx
+++ src/app/(home)/_components/nikan365Intro/index.tsx
@@ -22,11 +22,11 @@ const Nikan365Intro = ({data}: {
                                         h-[110px] flex flex-col items-center justify-center gap-3 lg:pl-4 lg:ml-4 pl-4 pb-4 lg:pb-0
                                         ${index === 0 ? "border-l lg:border-l lg:border-b-0 border-b" : ""}
                                         ${index === 1 ? " lg:border-l lg:border-b-0 border-b" : ""}
-                                        ${index === 2 ? "border-l lg:border-l lg:border-b-0 pt-4" : ""}
-                                        ${index === 3 ? "lg:border-b-0 pt-4" : ""}
+                                        ${index === 2 ? "border-l lg:border-l lg:border-b-0 lg:pt-0 pt-4 " : ""}
+                                        ${index === 3 ? "lg:border-b-0 pt-4 lg:pt-0" : ""}
                                         `}>
                                 <Image
-                                    className="w-full max-w-[66px] !relative"
+                                    className="w-full max-w-[66px] max-h-[60px] !relative"
                                     src={slide.image.original_url ?? ""}
                                     alt="slogen"
                                     fill

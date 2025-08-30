1f8cd4a2f123fa7bfc467ea45ca2e194757ed35c
AminEtemadifar <aminetemadifar.w@gmail.com>
2025-08-31 00:54:16 +0330

    debug

--- src/app/(home)/_components/virtualSlider/index.tsx
+++ src/app/(home)/_components/virtualSlider/index.tsx
@@ -1,25 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const virtualSlider = () => {
   return (
-    <div className="max-w-[1236px] container lg:mb-[150px]">
-      <Image
-        src={"/images/virtual_slider.png"}
-        alt=""
-        width={1209}
-        height={234}
-        quality={100}
-        className="mt-10 lg:mb-24 mb-10 object-cover md:block hidden mx-auto"
-      />{" "}
-      <Image
-        src={"/images/virtual_slider_mobile.png"}
-        alt=""
-        width={357}
-        height={269}
-        quality={100}
-        className="mt-10 mb-10 object-cover !relative block md:hidden mx-auto"
-      />
-    </div>
+      <Link href={"https://nikanvirtual.com/"} target={"_blank"}>
+          <div className="max-w-[1236px] container lg:mb-[150px]">
+              <Image
+                  src={"/images/virtual_slider.png"}
+                  alt=""
+                  width={1209}
+                  height={234}
+                  quality={100}
+                  className="mt-10 lg:mb-24 mb-10 object-cover md:block hidden mx-auto"
+              />{" "}
+              <Image
+                  src={"/images/virtual_slider_mobile.png"}
+                  alt=""
+                  width={357}
+                  height={269}
+                  quality={100}
+                  className="mt-10 mb-10 object-cover !relative block md:hidden mx-auto"
+              />
+          </div>
+      </Link>
   );
 };
 

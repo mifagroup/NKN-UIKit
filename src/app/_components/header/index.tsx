eb1291084b3764982ae835f333226a97621f2be5
AminEtemadifar <aminetemadifar.w@gmail.com>
2025-09-29 00:21:22 +0330

    lan switcher

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -2,7 +2,7 @@
 import { useMediaQuery, useTheme } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
-import { Links, MenuDrawer } from "./_components";
+import { Links, MenuDrawer , LangSwicher } from "./_components";
 import { useLanguage } from "@/contexts/LanguageContext";
 
 const Header = () => {
@@ -33,8 +33,8 @@ const Header = () => {
           </div>
           <div className="flex items-center gap-x-[22px]">
             <div className="flex items-center gap-x-1.5">
-              {/*<LangSwicher />
-               <LoginButton /> */}
+              <LangSwicher />
+               {/*<LoginButton /> */}
             </div>
             <MenuDrawer />
             <div
